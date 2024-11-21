a19d5aab9b32365955e4cc8e583fb0bd5d125a5e
bebedyk <144939714+bebedyk@users.noreply.github.com>
2024-11-21 08:30:06 +0100

    Add files via upload

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -1,6 +1,26 @@
+// Отримуємо елемент відео
+const video = document.getElementById('backgroundVideo');
 
+// Вимикаємо автоматичне відтворення і зациклення
+video.autoplay = false;
+video.loop = false;
 
-const icon = document.querySelector('.icon-menu');
-icon.addEventListener('click', function () {
-	document.documentElement.classList.toggle('menu-open');
+// Змінна для перевірки, чи відео завершилося
+let isVideoEnded = false;
+
+// Коли відео завершується
+video.addEventListener('ended', () => {
+    video.pause();
+    isVideoEnded = true; // Позначаємо, що відео завершилося
+    video.currentTime = video.duration - 0.1; // Ставимо на останній кадр
+});
+
+// Додаємо можливість запускати відео кліком
+video.addEventListener('click', () => {
+    if (isVideoEnded) {
+        // Якщо відео завершилося, запускаємо з початку
+        video.currentTime = 0;
+        isVideoEnded = false; // Скидаємо стан завершення
+    }
+    video.play(); // Відтворюємо
 });
